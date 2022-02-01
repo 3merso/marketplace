01f6b173c78ab7d00aba636530b512ac16601955
emartin5 <emersonsm@icloud.com>
2022-02-01 18:43:04 -0300

    Criação de migration de stores e users

---
+++ database/migrations/2022_02_01_212610_create_stores_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('name');
+            $table->string('description');
+            $table->string('phone');
+            $table->string('mobile_phone');
+            $table->string('slug');
+
+            $table->timestamps();
+            // nome da chave estrangeira: stores_user_id_foreign
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
